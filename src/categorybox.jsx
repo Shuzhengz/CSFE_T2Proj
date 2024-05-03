5050adf18981764c5ac08860e6fabeeb958ddbd7
yechanpark <yechanpark07@gmail.com>
2024-05-02 21:42:27 -0700

    categories made

---
+++ src/categorybox.jsx
@@ -0,0 +1,15 @@
+
+export function CategoryBox ({title}){
+    return (
+        <div className='category' onClick={() => alert('Redirect to item')}>
+            <header>{title}</header> 
+            <p>
+                pasdoifjapsdoifjapsdoifjapsdoifjapsodfijapsdofijapsdoifjaspdfoijasdpfoijasdpfoijasdfpoiasjdpfoijasdopfjaspdofijaposdifj 
+                 {title}
+
+            </p>
+        
+            
+        </div>
+    )
+}
--- src/majors-categories.jsx
+++ src/majors-categories.jsx
@@ -1,9 +1,39 @@
-
+import {CategoryBox} from './categorybox.jsx'
 export function Majors() {
     return (
-        <div>
-            <p>hi</p>
+        <>
+        
+        <div className='category-container'>
+            <CategoryBox
+            title='computer science'
+            />
+            <CategoryBox
+            title='biology'
+            />
+            <CategoryBox
+            title='hee hee'
+            />
+            <CategoryBox
+            title='math'
+            />
+            
         </div>
+        <div className='category-container'>
+            <CategoryBox
+                title='engineering'
+                />
+            <CategoryBox
+                title='health'
+                />
+            <CategoryBox
+                title='nursing'
+                />
+            <CategoryBox
+                title='physics'
+                />
+        </div>
+        
+        </>
+        
     )
 }
-
